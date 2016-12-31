b9a642be3212a0e5a87ae4801543c3b29eb3f6d9
mika-g <dakglowa@gmail.com>
2016-12-31 15:15:32 +0000

    Homework 2 answers updated

--- angularJS/homework2/app.js
+++ angularJS/homework2/app.js
@@ -5,46 +5,51 @@
   .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
   .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
+
   ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
   AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
 
   function ToBuyShoppingController(ShoppingListCheckOffService) {
-    var controller = this;
+    var shopping = this;
 
-    this.shopping_list = ShoppingListCheckOffService.getshopping_list();
+    controller.shoppingitems = ShoppingListCheckOffService.shoppingitems();
 
-    this.shoppingItem = function(index) {
-      ShoppingListCheckOffService.shoppingItem(index);
+    controller.shop = function(index) {
+      ShoppingListCheckOffService.shop(index);
     }
   }
 
+
   function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
     var bought = this;
-    bought.list = ShoppingListCheckOffService.getbought_list();
+
+    controller.boughtitems = ShoppingListCheckOffService.getboughtitems();
+
   }
 
   function ShoppingListCheckOffService() {
     var service = this;
-    var bought_list = [];
-    var shopping_list = [
-      {name: "Bread", quantity: 1},
-      {name: "Milk", quantity: 1},
-      {name: "Eggs", quantity: 12},
-      {name: "Butter", quantity: 2},
-      {name: "Orange Juice", quantity: 1}
-    ];
 
-    this.getShoppingList = function() {
-      return shopping_list;
-    };
+    var shoppingitems = [{ name: "cookies", quantity: 10 },
+      { name: "milk", quantity: 10 },
+      { name: "tea", quantity: 10 },
+      { name: "coffee", quantity: 10 },
+      { name: "sugar", quantity: 10 }]
+    var boughtitems = [];
 
-    this.getboughtList = function() {
-      return bought_list;
-    };
+    this.shop = function (index) {
+      boughtitems.push(toBuyItems[index]);
+      shoppingitems.splice(index, 1);
+    }
 
-    this.shoppingItem = function(itemIndex) {
-      var item = shopping_list.splice(itemIndex, 1);
-      bought_list.push(item[0]);
-    };
+    this.getshoppingitems = function() {
+      return toBuyItems;
+    }
+
+    this.getboughtitems = function() {
+      return boughtItems;
+    }
   }
-})();
+
+}
+)();
